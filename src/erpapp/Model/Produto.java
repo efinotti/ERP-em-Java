59d3e4198da19e16c52025cc10aad92d535ccb5d
ueg <ueg@10.90.0.222>
2026-05-27 21:16:17 -0300

    "Implementando funções de Produto"

--- src/erpapp/Model/Produto.java
+++ src/erpapp/Model/Produto.java
@@ -1,11 +1,49 @@
 package erpapp.Model;
 
+import java.util.ArrayList;
+
 public class Produto {
     private int id;
     private String nome;
     private float preco;
     private int quantidade;
+    private ArrayList<Produto> listaProduto = new ArrayList<>();
     
+    
+    public void incluir(Produto p){
+        listaProduto.add(p);
+        armazenar();
+    }
+    
+    public void consultar(){
+        String termo = JOptionPane.showInputDialog(view,"Digite o nome do produto para buscar...");
+        if(termo != null && !termo.trim().isEmpty()){
+            listModel.clear();
+            for(Produto p : model.listaProduto){
+                if(p.getNome().toLowerCase().contains(termo.toLowerCase())){
+                    listModel.addElement(p);
+                }
+            }
+        if(listModel.isEmpty()){
+            JOptionPane.showMessageDialog(view,"Nenhum produto encontrado.");
+        }
+        }else{
+            return;
+        }
+    }
+    
+    public void listar(){
+        
+    }
+    
+    public void alterar(){
+        
+    }
+    
+    public void remover(Produto p){
+        listaProduto.remove(p);
+        armazenar();
+    }
     /**
      * @return the id
      */
